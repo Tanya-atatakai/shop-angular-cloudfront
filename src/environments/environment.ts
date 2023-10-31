2b3a500518b92e96f6af078fea83f2381e65546b
Tatiana <tatiana_priemova_epam.com>
2023-10-31 15:48:57 +0400

    Update API links

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment: Config = {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://93b6cby0df.execute-api.us-east-1.amazonaws.com/dev',
-    bff: 'https://vhbsrzp2dg.execute-api.us-east-1.amazonaws.com/dev',
+    bff: 'https://9dy714phzi.execute-api.us-east-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
